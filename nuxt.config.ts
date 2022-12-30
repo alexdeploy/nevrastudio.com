b3597f0281688fdb5ea450897055d881cb5ed7c5
alexruedadev <me@alexrueda.dev>
2022-12-30 17:44:43 +0100

    Testing sharing card image smaller size

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,14 @@
 export default defineNuxtConfig({
     app: {
         head: {
-            title: 'nevra studio',
+            title: 'nevra studio', /* Nav title */
             meta: [
                 { charset: 'utf-8' },
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' },
                 { hid: 'description', name: 'description', content: 'Nevra Studio' },
-                { hid: 'og:title', property: 'og:title', content: 'nevra studio' }, /* Share card title */
-                { hid: 'og:description', property: 'og:description', content: 'Nevra Studio - Interior Design' }, /* Share card description */
-                { hid: 'og:image', property: 'og:image', content: 'og-image.jpg' } /* Share card image */
+                { hid: 'og:title', property: 'og:title', content: 'nevra studio' }, /* Sharing card title */
+                { hid: 'og:description', property: 'og:description', content: 'Nevra Studio - Interior Design' }, /* Sharing card description */
+                { hid: 'og:image', property: 'og:image', content: 'og-image-small.jpg' } /* Sharing card image */
             ]
         }
     },
